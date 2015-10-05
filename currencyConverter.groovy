6212d69882b50fe74e1454994801ed38ebfdb514
Jose Massada <reversed@prym.net>
2015-10-05 19:04:36 +0100

    added comment header and did minor cleanup

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,9 +1,20 @@
+// Currency converter
+//
+// Calculates the convertion rate betweeen pounds and euros and vice-versa.
+//
+
 print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
+
 print "If I give you 1€, how many Sterling pounds will you give me? ";
 double poundOverEuroRatio = Double.parseDouble(System.console().readLine());
+
 println "OK, now I know enough.";
+
+// start
+
 boolean finished = false;
+
 while (!finished) {
     println "";
     println "What would you like to do?";
@@ -12,26 +23,28 @@ while (!finished) {
     println "   0 - Exit the program";
     println "";
     print   "> ";
+	
     int choice = Integer.parseInt(System.console().readLine());
     switch (choice) {
+	case 0: 
+	    finished = true;
+	    break;
     case 1:
-	  print "How many pounds would you like to convert? ";
-	  double pounds = Double.parseDouble(System.console().readLine());
-	  double euro   = pounds * euroOverPoundRatio;
-	  println "£" + pounds + " will give you " + euro + "€";
-	  break;
+	    print "How many pounds would you like to convert? ";
+	    double pounds = Double.parseDouble(System.console().readLine());
+	    double euro   = pounds * euroOverPoundRatio;
+	    println "£" + pounds + " will give you " + euro + "€";
+	    break;
     case 2: 
-	  print "How many euro would you like to convert? ";
-	  double euro   = Double.parseDouble(System.console().readLine());
-	  double pounds = euro * poundOverEuroRatio;
-	  println euro + "€ will give you £" + pounds;
-	  break;
-    case 0: 
-	  finished = true;
-	  break;
+	    print "How many euro would you like to convert? ";
+	    double euro   = Double.parseDouble(System.console().readLine());
+	    double pounds = euro * poundOverEuroRatio;
+	    println euro + "€ will give you £" + pounds;
+	    break;
     default: 
-	  println "Sorry, that is not a valid option";
+	    println "Sorry, that is not a valid option";
+		break;
     }	  
 }
-println "Exiting the program";
 
+println "Exiting the program";
